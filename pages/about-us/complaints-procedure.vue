3df32e9360b8df77edfac6ef99a2cd198c8d5306
Liam Potter <liam-p@fostr.io>
2021-04-07 13:59:03 +0100

    complaints page

--- pages/about-us/complaints-procedure.vue
+++ pages/about-us/complaints-procedure.vue
@@ -1,5 +1,150 @@
 <template>
-  <p>Complaints Procedure</p>
+  <div>
+    <h1>Complaints Policy &amp; Procedure</h1>
+
+    <h2>Our Policy is:</h2>
+    <ul
+      class="list-disc pl-5"
+      role="list"
+    >
+      <li>
+        <p>To provide a fair complaints procedure which is clear and easy to use for anyone wishing to make a complaint.</p>
+      </li>
+      <li>
+        <p>To publicise the existence of our complaints procedure so that people know how to contact us to make a complaint.</p>
+      </li>
+      <li>
+        <p>To make sure everyone at Newcastle City Juniors knows what to do if a complaint is received.</p>
+      </li>
+      <li>
+        <p>To make sure all complaints are investigated fairly and in a timely way.</p>
+      </li>
+      <li>
+        <p>To make sure that complaints are, wherever possible, resolved and relationships are repaired.</p>
+      </li>
+      <li>
+        <p>To gather information which helps us to improve what we do.</p>
+      </li>
+    </ul>
+
+    <h2 class="mt-8">
+      Definition of a Complaint
+    </h2>
+    <p>A complaint is any expression of dissatisfaction, whether justified or not, about any aspect of Newcastle City Juniors.</p>
+
+    <h2 class="mt-8">
+      Dealing with complaints – initial concerns (informal process)
+    </h2>
+    <p class="mb-4">
+      Newcastle City Juniors take informal concerns seriously with the hope of the concern not developing into a formal complaint. However, formal complaints should always follow the complaints procedure.
+    </p>
+    <p class="mb-4">
+      In most cases the ‘Team Manager’ (e.g.U9s Team Coach) or the individual delivering the coaching orteaching session, may often receive the first approach. It would be helpful if staff were able to resolve issues on the spot. Recognising an error and apologising (without prejudice) where necessary may result in a swift and satisfactory resolution.
+    </p>
+    <p class="mb-4">
+      If a concern or complaint has been made a committee member will telephone the complainant to gain greater clarification of the situation or in some cases it may be that a parent wishes to request a meeting with a committee member. Any such request should be made via the club welfare officers, preferably in writing. The request will need to include sufficient details to allow the clubs welfare officers to decide whether a meeting is necessary, who should be asked to attend the meeting and to make any necessary preparations.
+    </p>
+    <p class="mb-4">
+      Any person, player or parent may make a formal safeguarding complaint at any time by putting the complaint in writing to the club. Complaints should go to Club Welfare Officers.
+    </p>
+    <p class="mb-4">
+      If the complaint relates to a specific person, they should be informed and given a fair opportunity to respond. The person handling the complaint within a week should acknowledge complaints. The acknowledgement should say who is dealing with the complaint and when the person complaining can expect a reply. A copy of this complaints procedure should be attached.
+    </p>
+    <p>
+      Ideally complainants should receive a reply within 7 days and a definitive reply within 4 weeks. If this is not possible because for example, an investigation has not been fully completed, a progress report should be sent with an indication of when a full reply will be given.
+    </p>
+
+    <h2 class="mt-8">
+      Dealing with safeguarding complaints – formal procedures
+    </h2>
+    <p class="mb-4">
+      <strong>The formal procedures</strong> will need to be invoked when initial attempts to resolve the issue are unsuccessful and the person raising the concern remains dissatisfied and wishes to take the matter further. The request for a formal referral should be acknowledged within a week of receiving it. The acknowledgement should say who will deal with the case and when the complainant can expect a reply. An FA referral form should be sent to the complainant.
+    </p>
+    <p class="mb-4">
+      <strong>The complainant should be asked to clarify their complaint by putting it in writing</strong> and advised to clearly identify the nature of the complaint, including examples of poor practice; details of those involved; dates; times and venue where the incident(s) occurred if possible.
+    </p>
+    <p>The clubs welfare officers have a responsibility for the management of the clubs safeguarding complaints procedure. They may call together a panel of appropriate people to deal with any formal complaint, which is made. Dependent upon the nature of the complaint and who is involved, this may include the vice-chairman and club welfare officers. The chairman (Stephen Chambers) would not be included at this stage as they may be required to have a role in any appeals procedure. <strong>Any member of staff or volunteer cited in a complaint must not be the person to whom a written complaint is passed to deal with.</strong></p>
+
+    <h2 class="mt-8">
+      Child Protection within Safeguarding
+    </h2>
+    <p>The club will refer to the appropriate statutory agencies and football authorities any child protection concern, allegation or disclosure regardless of the timescale of receipt of the information. Historical allegations of child abuse must be referred to the Police.</p>
+
+    <h2 class="mt-8">
+      Resolving Complaints
+    </h2>
+    <p class="mb-4">
+      Newcastle City Juniors will keep in mind ways in which a complaint can be resolved. It might be sufficient to acknowledge that the complaint is valid in whole or in part. In addition is may be appropriate to offer one or more of the following:
+    </p>
+    <ul
+      class="list-disc pl-5 mb-4"
+      role="list"
+    >
+      <li>
+        <p>An apology.</p>
+      </li>
+      <li>
+        <p>An explanation.</p>
+      </li>
+      <li>
+        <p>An admission that the situation could have been handled differently.</p>
+      </li>
+      <li>
+        <p>An assurance that the event complained of will not happen again.</p>
+      </li>
+      <li>
+        <p>An explanation of the steps that have or are to be taken to ensure that it will not happen again.</p>
+      </li>
+      <li>
+        <p>An undertaking to review club policies in light of the complaint.</p>
+      </li>
+      <li>
+        <p>All the above MUST be recorded.</p>
+      </li>
+    </ul>
+    <p class="mb-4">
+      It is helpful if complainants are encouraged to state what actions they feel might resolve the problem at any stage. An admission that the club could have handled the situation better is not the same as an admission of negligence.
+    </p>
+    <p class="mb-4">
+      An effective procedure will identify areas of agreement between parties. It is also of equal importance to clarify any misunderstanding that might have occurred as this can create a positive atmosphere in which to discuss any outstanding issues.
+    </p>
+    <p>
+      It is important to note however, that should a complainant raise an entirely new, separate complaint, it must be responded to in accordance with the Complaints Procedure.
+    </p>
+
+    <h2 class="mt-8">
+      Contact Information
+    </h2>
+    <p class="mb-4">
+      <strong>Newcastle City Juniors Welfare Officer:</strong><br>Name: Keith Morris<br>Telephone: <a href="tel:+447914118197">07914 118197</a><br>Email: <a href="mailto:keithmorris_cyd@hotmail.co.uk">keithmorris_cyd@hotmail.co.uk</a>
+    </p>
+    <p class="mb-4">
+      <strong>Newcastle City Juniors Assistant Welfare Officer:</strong><br>Name: Adam Jones<br>Telephone: <a href="tel:+447518389898">07518 389898</a><br>Email: <a href="mailto:ncj.adamjones@hotmail.com">ncj.adamjones@hotmail.com</a>
+    </p>
+    <p class="mb-4">
+      <strong>Newcastle City Juniors Assistant Welfare Officer:</strong><br>Name: Jack Foalle<br>Telephone: <a href="tel:+447432079000">07432 079000</a><br>Email: <a href="mailto:jackfoalle1994@yahoo.co.uk">jackfoalle1994@yahoo.co.uk</a>
+    </p>
+    <p class="mb-4">
+      If at any time you are not able to contact your Club Welfare Officer or the matter is clearly serious then you can either:
+    </p>
+    <p class="mb-4">
+      <strong>Northumberland County Football Association’s Welfare Officers:</strong><br>Name: Ray Johnstone<br>Telephone: <a href="tel:+441912700700">0191 2700 700</a><br>Email: <a href="mailto:ray.johnstone@northumberlandfa.com">ray.johnstone@northumberlandfa.com</a>
+    </p>
+    <ul
+      class="list-disc pl-5"
+      role="list"
+    >
+      <li>
+        <p><a href="https://www.thefa.com/football-rules-governance/safeguarding">https://www.TheFA.com/football-rules-governance/safeguarding</a></p>
+      </li>
+      <li>
+        <p>Emailing - <a href="mailto:safeguarding@thefa.com">Safeguarding@TheFA.com</a></p>
+      </li>
+      <li>
+        <p>The FA Safeguarding Children general enquiry line <a href="tel:+448452108080">0845 210 8080</a></p>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
