941a8fc2c1b5e6b88bc50ec31d92612ba75a7cfc
Liam Potter <liam-p@fostr.io>
2021-04-25 17:24:22 +0100

    add some line height to text on home hero on larger screens

--- components/home/HomeHero.vue
+++ components/home/HomeHero.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home-hero flex justify-center items-center bg-center bg-cover">
     <div class="text-white bg-black bg-opacity-50 flex justify-center items-center font-serif italic h-full w-full px-4 py-8 lg:py-36">
-      <div class="text-2xl text-center lg:text-5xl lg:w-7/12">
+      <div class="text-2xl text-center lg:text-5xl lg:w-7/12 lg:leading-snug">
         <p>Our Clubs Mission Is To Attract, Develop, Motivate &amp; Produce The Best Natural Talent In The North East By Providing The Best Possible Experience...</p>
         <p class="text-gold">
           The "City Experience"
