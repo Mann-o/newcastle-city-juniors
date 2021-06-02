e7eeb2c94ca47b29db9725de839d5add5aa0d95a
Liam Potter <liam-p@fostr.io>
2021-06-02 09:07:29 +0100

    add manifest config

--- config/pwa.ts
+++ config/pwa.ts
@@ -1,6 +1,9 @@
 export const pwa = {
   manifest: {
     lang: 'en',
+    name: 'Newcastle City Juniors',
+    short_name: 'NCJ',
+    description: 'Official website of Newcastle City Juniors F.C. Formed in the summer of 2005 based in the west end of Newcastle upon Tyne, we are committed to coaching players of the future. Registered charity number 1132738.',
   },
   meta: {
     name: 'Newcastle City Juniors',
