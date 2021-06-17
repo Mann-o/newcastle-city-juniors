0b6466d018841d418f06d75adb50bb5382411fbc
Liam Potter <liam-p@fostr.io>
2021-06-17 09:34:20 +0100

    slight update to presentation update post

--- pages/news-and-events/presentation-2020-update.vue
+++ pages/news-and-events/presentation-2020-update.vue
@@ -2,10 +2,10 @@
   <div>
     <h1>NCJ Presentation - 2020-21 Season - Update</h1>
 
-    <p class="pb-4">Following the latest government announcement on Monday this years club presentation has had to be altered slightly unfortunately.</p>
-    <p class="pb-4">Originally the football club had planned to hold this years presentation indoors inside the clubs sports hall but following the latest update we will move the presentation outdoors ensuring all the children still get their trophies which they all deserve following a tough and very much interrupted season due to COVID-19.</p>
-    <p>The structure has slightly changed with each age group been allocated a time slot and we ask for all players, parents and coaches to be on site at least 10 minutes prior to your age group/teams presentation starting.</p>
-    <p>The schedule for Saturday 26th June 2021 is listed below:</p>
+    <p class="pb-4">Following the latest government announcement on Monday this year's club presentation has had to be altered slightly, unfortunately.</p>
+    <p class="pb-4">Originally the football club had planned to hold this year's presentation indoors inside the clubs sports hall but following the latest update, we will move the presentation outdoors ensuring all the children still get their trophies which they all deserve following a tough and very much interrupted season due to COVID-19.</p>
+    <p>The structure has slightly changed with each age group being allocated a time slot and we ask for all players, parents and coaches to be on site at least 10 minutes prior to your age group/teams presentation starting.</p>
+    <p>The new schedule for Saturday 26th June 2021 is listed below:</p>
 
     <h2 class="mt-8">Timetable</h2>
     <ul class="list-disc list-inside pb-4">
@@ -23,8 +23,10 @@
       <li>6.00pm - Euro 2021 Game</li>
     </ul>
 
-    <p class="pt-4 pb-4">For all parents/guardians who have already purchased tickets and paid money online they/you will receive a full refund and this will be in their/your bank account within 4 working days.</p>
-    <p class="pb-4">The football clubs Juve Longue is open throughout the day with refreshments, hot and cold food and alcoholic beverages been served throughout. We would welcome you all stay and celebrate your child's success this season and we will be showing the Euro 2021 Knockout games live on the day.</p>
+    <p class="py-4">Each year we want to recognise all of the special talent we have here at Newcastle City Juniors, and present all of the children with a participation trophy for the season.</p>
+    <p class="pb-4">All attending players <strong>MUST</strong> wear their home stripe (Black/White Top, White Shorts/Socks) on the day. All players will be asked to hand in their Black/White Top at the end of the presentation by their team coaches so please bring a change of top for your child. White Shorts/Socks can be kept.</p>
+    <p class="pb-4">For all parents/guardians who have already purchased tickets and paid money online, they/you will receive a full refund and this will be in their/your bank account within 5-10 working days. You may have already received an email from our online payments provider, Stripe, confirming this refund.</p>
+    <p class="pb-4">The football clubs Juve Longue is open throughout the day with refreshments, hot and cold food and alcoholic beverages being served throughout. We would welcome you all stay and celebrate your child's success this season and we will be showing the Euro 2021 Knockout games live on the day.</p>
     <p>We hope to be able to celebrate the 2021/22 season as one big club next Summer.</p>
 
   </div>
