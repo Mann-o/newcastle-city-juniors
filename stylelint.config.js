fab9fb49600b5bccbd360f21d1fc097784a4df50
Liam Potter <liam-p@fostr.io>
2021-12-05 14:51:49 +0000

    update dependencies

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,9 +1,8 @@
 module.exports = {
   extends: [
-    'stylelint-config-standard'
+    'stylelint-config-vue'
   ],
-  // add your custom config here
-  // https://stylelint.io/user-guide/configuration
+
   rules: {
     'at-rule-no-unknown': [
       true,
