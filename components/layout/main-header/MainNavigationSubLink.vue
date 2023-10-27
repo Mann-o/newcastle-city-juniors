529648b8ca7933c0c13e5b92255d860d242eec36
Liam Potter <me@liam-potter.co.uk>
2023-10-27 23:35:26 +0100

    add external link support to sublinks

--- components/layout/main-header/MainNavigationSubLink.vue
+++ components/layout/main-header/MainNavigationSubLink.vue
@@ -1,6 +1,15 @@
 <template>
   <li class="pl-4 py-2 text-sm border-b border-grey-800 last:border-b-0 lg:pr-4 lg:whitespace-nowrap lg:transition-all">
+    <a
+      v-if="childRoute.external"
+      :href="route.to"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      {{ childRoute.label }}
+    </a>
     <NuxtLink
+      v-else
       class="block text-white"
       :to="childRoute.to"
       @click.prevent="$emit('navigate')"
