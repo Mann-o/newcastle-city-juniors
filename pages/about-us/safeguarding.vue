ca665ce688c5c7128fdd31e083a404cd2b3bf5e0
Liam Potter <me@liam-potter.co.uk>
2023-10-27 23:07:42 +0100

    update safeguarding policy

--- pages/about-us/safeguarding.vue
+++ pages/about-us/safeguarding.vue
@@ -8,7 +8,7 @@
     >
       <li class="mb-8">
         <p class="mb-4">
-          Newcastle City Juniors acknowledges its responsibility to safeguard the welfare of every child and young person who has been entrusted to its care and is committed to working to provide a safe environment for all members. A child or young person is anyone under the age of 18 engaged in any club football activity. We subscribe to The Football Association’s (The FA) Safeguarding Children – Policy and Procedures and endorse and adopt the Policy Statement contained in that document.
+          Newcastle City Juniors acknowledges its responsibility to safeguard the welfare of every child and young person who has been entrusted to its care and is committed to working to provide a safe environment for all members. A child or young person is anyone under the age of 18 engaged in any club football activity. We subscribe to The Football Association's (The FA) Safeguarding Children - Policy and Procedures and endorse and adopt the Policy Statement contained in that document.
         </p>
         <p class="mb-4">
           The key principles of The FA Safeguarding Children Policy are that:
@@ -18,7 +18,7 @@
           role="list"
         >
           <li>
-            <p>the child’s welfare is, and must always be, the paramount consideration.</p>
+            <p>the child's welfare is, and must always be, the paramount consideration.</p>
           </li>
           <li>
             <p>all children and young people have a right to be protected from abuse regardless of their age, gender, disability, race, sexual orientation, faith or belief.</p>
@@ -37,12 +37,12 @@
       </li>
 
       <li class="mb-8">
-        <p>Newcastle City Juniors has a role to play in safeguarding the welfare of all children and young people by protecting them from physical, sexual or emotional harm and from neglect or bullying. It is noted and accepted that The Football Association’s Safeguarding Children Regulations (see The FA Handbook) applies to everyone in football whether in a paid or voluntary capacity. This means whether you are a volunteer, match official, helper on club tours, football coach, club official or medical staff.</p>
+        <p>Newcastle City Juniors has a role to play in safeguarding the welfare of all children and young people by protecting them from physical, sexual or emotional harm and from neglect or bullying. It is noted and accepted that The Football Association's Safeguarding Children Regulations (see The FA Handbook) applies to everyone in football whether in a paid or voluntary capacity. This means whether you are a volunteer, match official, helper on club tours, football coach, club official or medical staff.</p>
       </li>
 
       <li class="mb-8">
         <p class="mb-4">
-          We endorse and adopt The FA’s Responsible Recruitment guidelines for recruiting volunteers and we will:
+          We endorse and adopt The FA's Responsible Recruitment guidelines for recruiting volunteers and we will:
         </p>
         <ul
           class="list-disc pl-5 mb-4"
@@ -70,19 +70,19 @@
         <p class="mb-4">
           If there are concerns regarding the appropriateness of an individual who is already involved or who has approached us to become part of Newcastle City Juniors guidance will be sought from The Football Association. It is noted and accepted that The FA will consider the relevance and significance of the information obtained via the CRC Process and that all suitability decisions will be made in accordance with legislation and in the best interests of children and young people.
         </p>
-        <p>It is accepted that The FA aims to prevent people with a history of relevant and significant offending from having contact with children or young people and the opportunity to influence policies or practice with children or young people. This is to prevent direct sexual or physical harm to children and to minimise the risk of ‘grooming’ within football.</p>
+        <p>It is accepted that The FA aims to prevent people with a history of relevant and significant offending from having contact with children or young people and the opportunity to influence policies or practice with children or young people. This is to prevent direct sexual or physical harm to children and to minimise the risk of 'grooming' within football.</p>
       </li>
 
       <li class="mb-8">
-        <p>Newcastle City Juniors supports The FA’s Whistle Blowing Policy. Any adult or young person with concerns about an adult in a position of trust with football can ‘whistle blow’ by contacting The FA Safeguarding Team on <a href="tel:+448001691863">0800 169 1863</a>, by writing to The FA Case Manager at The Football Association, Wembley Stadium, PO Box 1966, London SW1P 9EQ, by emailing <a href="mailto:safeguarding@thefa.com">Safeguarding@TheFA.com</a> or alternatively by going direct to the Police, Children’s Social Care or the NSPCC. Newcastle City Juniors encourages everyone to know about The FA’s Whistle Blowing Policy and to utilise it if necessary.</p>
+        <p>Newcastle City Juniors supports The FA's Whistle Blowing Policy. Any adult or young person with concerns about an adult in a position of trust with football can 'whistle blow' by contacting The FA Safeguarding Team on <a href="tel:+448001691863">0800 169 1863</a>, by writing to The FA Case Manager at The Football Association, Wembley Stadium, PO Box 1966, London SW1P 9EQ, by emailing <a href="mailto:safeguarding@thefa.com">Safeguarding@TheFA.com</a> or alternatively by going direct to the Police, Children's Social Care or the NSPCC. Newcastle City Juniors encourages everyone to know about The FA's Whistle Blowing Policy and to utilise it if necessary.</p>
       </li>
 
       <li class="mb-8">
-        <p>Newcastle City Juniors has appointed a Club Welfare Officer in line with The FA’s role profile and required completion of the Safeguarding Children and Welfare Officers Workshop. The post holder will be involved with Welfare Officer training provided by The FA and/or County FA. The Club Welfare Officer is the first point of contact for all club members regarding concerns about the welfare of any child or young person. The Club Welfare Officer will liaise directly with the County FA (CFA) Welfare Officer and will be familiar with the procedures for referring any concerns. They will also play a proactive role in increasing awareness of Respect, poor practice and abuse amongst club members.</p>
+        <p>Newcastle City Juniors has appointed a Club Welfare Officer in line with The FA's role profile and required completion of the Safeguarding Children and Welfare Officers Workshop. The post holder will be involved with Welfare Officer training provided by The FA and/or County FA. The Club Welfare Officer is the first point of contact for all club members regarding concerns about the welfare of any child or young person. The Club Welfare Officer will liaise directly with the County FA (CFA) Welfare Officer and will be familiar with the procedures for referring any concerns. They will also play a proactive role in increasing awareness of Respect, poor practice and abuse amongst club members.</p>
       </li>
 
       <li class="mb-8">
-        <p>We acknowledge and endorse The FA’s identification of bullying as a category of abuse. Bullying of any kind is not acceptable at our club. If bullying does occur, all players or parents/carers should be able to tell and know that incidents will be dealt with promptly. Incidents need to be reported to the Club Welfare Officer in cases of serious bullying the CFA Welfare Officer may be contacted.</p>
+        <p>We acknowledge and endorse The FA's identification of bullying as a category of abuse. Bullying of any kind is not acceptable at our club. If bullying does occur, all players or parents/carers should be able to tell and know that incidents will be dealt with promptly. Incidents need to be reported to the Club Welfare Officer in cases of serious bullying the CFA Welfare Officer may be contacted.</p>
       </li>
 
       <li class="mb-8">
@@ -91,7 +91,7 @@
 
       <li class="mb-8">
         <p class="mb-4">
-          Reporting your concerns about the welfare of a child or young person. Safeguarding is everyone’s responsibility if you are worried about a child it is important that you report your concerns – no action is not an option.
+          Reporting your concerns about the welfare of a child or young person. Safeguarding is everyones responsibility if you are worried about a child it is important that you report your concerns - no action is not an option.
         </p>
         <ol
           class="list-lower-alpha pl-5 mb-4"
@@ -112,7 +112,7 @@
             </ul>
           </li>
           <li>
-            <p>If the concern is more serious – possible child abuse, where possible, contact the CFA Welfare Officer first, then immediately contact the Police or Children’s Social Care.</p>
+            <p>If the concern is more serious - possible child abuse, where possible, contact the CFA Welfare Officer first, then immediately contact the Police or Children's Social Care.</p>
           </li>
           <li>
             <p>If the child needs immediate medical treatment take them to a hospital or call an ambulance and tell them this is a child protection concern. Let your Club Welfare Officer know what action you have taken, they in turn will inform the CFA Welfare Officer.</p>
@@ -130,7 +130,7 @@
                 <p>contact The FA Safeguarding Team on <a href="tel:+448001691863">0800 169 1863</a> or <a href="mailto:safeguarding@thefa.com">Safeguarding@TheFA.com</a></p>
               </li>
               <li>
-                <p>contact the Police or Children’s Social Care</p>
+                <p>contact the Police or Children's Social Care</p>
               </li>
               <li>
                 <p>call the NSPCC 24 hour Helpline for advice on <a href="tel:+448088005000">0808 800 5000</a> or text <a href="sms:88858">88858</a> or email <a href="mailto:help@nspcc.org.uk">help@nspcc.org.uk</a></p>
@@ -138,7 +138,7 @@
             </ul>
           </li>
         </ol>
-        <p>NB – The FA’s Safeguarding Children Policy and Procedures are available via – <a href="https://www.thefa.com/football-rules-governance/safeguarding">https://www.TheFA.com/football-rules-governance/safeguarding</a> – click on ‘Raising Awareness – Best Practice Downloads’, the Policy and Procedures document is within the resources area. The policy outlines in detail what to do if you are concerned about the welfare of a child and includes flow diagrams which describe this process. How to make a referral is also covered in the Safeguarding Children workshop. Participants are given the opportunity to discuss how this feels and how best they can prepare themselves to deal with such a situation. For more information on this workshop contact your County Welfare Officer.</p>
+        <p>NB - The FA's Safeguarding Children Policy and Procedures are available via - <a href="https://www.thefa.com/football-rules-governance/safeguarding">https://www.TheFA.com/football-rules-governance/safeguarding</a> - click on 'Raising Awareness - Best Practice Downloads', the Policy and Procedures document is within the resources area. The policy outlines in detail what to do if you are concerned about the welfare of a child and includes flow diagrams which describe this process. How to make a referral is also covered in the Safeguarding Children workshop. Participants are given the opportunity to discuss how this feels and how best they can prepare themselves to deal with such a situation. For more information on this workshop contact your County Welfare Officer.</p>
       </li>
 
       <li>
@@ -146,10 +146,7 @@
           Further advice on Safeguarding Children matters can be obtained from:
         </p>
         <p class="mb-4">
-          <strong>Newcastle City Juniors Welfare Officer:</strong><br>Name: Keith Morris<br>Telephone: <a href="tel:+447914118197">07914 118197</a><br>Email: <a href="mailto:keithmorris_cyd@hotmail.co.uk">keithmorris_cyd@hotmail.co.uk</a>
-        </p>
-        <p class="mb-4">
-          <strong>Newcastle City Juniors Assistant Welfare Officer:</strong><br>Name: Adam Jones<br>Telephone: <a href="tel:+447518389898">07518 389898</a><br>Email: <a href="mailto:ncj.adamjones@hotmail.com">ncj.adamjones@hotmail.com</a>
+          <strong>Newcastle City Juniors Welfare Officer:</strong><br>Name: Adam Jones<br>Telephone: <a href="tel:+447518389898">07518 389898</a><br>Email: <a href="mailto:ncj.adamjones@hotmail.com">ncj.adamjones@hotmail.com</a>
         </p>
         <p class="mb-4">
           <strong>Newcastle City Juniors Assistant Welfare Officer:</strong><br>Name: Jack Foalle<br>Telephone: <a href="tel:+447432079000">07432 079000</a><br>Email: <a href="mailto:jackfoalle1994@yahoo.co.uk">jackfoalle1994@yahoo.co.uk</a>
@@ -158,7 +155,7 @@
           If at any time you are not able to contact your Club Welfare Officer or the matter is clearly serious then you can either:
         </p>
         <p class="mb-4">
-          <strong>Northumberland County Football Association’s Welfare Officers:</strong><br>Name: Ray Johnstone<br>Telephone: <a href="tel:+441912700700">0191 2700 700</a><br>Email: <a href="mailto:ray.johnstone@northumberlandfa.com">ray.johnstone@northumberlandfa.com</a>
+          <strong>Northumberland County Football Association's Safeguarding Team:</strong><br>Telephone: <a href="tel:+441912700700">0191 2700 700</a><br>Email: <a href="mailto:safeguarding@northumberlandfa.com">safeguarding@northumberlandfa.com</a>
         </p>
         <ul
           class="list-disc pl-5"
